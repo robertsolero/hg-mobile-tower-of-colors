f06a8ea5d0bd0d13091085c89218985ba556dee3
Robert Solero <robert.solero@popcore.com>
2024-02-28 20:02:11 +0100

    Convert BoosterManager into BoosterUsageRequester

---
+++ Assets/3_Scripts/Boosters/BoosterUsageRequester.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoosterUsageRequester : MonoBehaviour
+{
+    [SerializeField]
+    private List<BoosterData> boosters = new List<BoosterData>();
+    [SerializeField] 
+    private BoosterUsageRequestUI boosterUsageRequestUIPrefab;
+    [SerializeField] 
+    private Transform uiContainer;  
+
+    public void Start()
+    {
+        GameManager.Instance.OnAvailableBallAmountChanged += OnAvailableBallAmountChanged;
+    }
+    
+    private void OnAvailableBallAmountChanged(int amountOfBallsAvailable)
+    {
+        var moreBallsBooster = boosters.Find(x => x.BoosterType == BoosterType.MoreBalls);
+        if (moreBallsBooster == null)
+            return;
+        
+        if (amountOfBallsAvailable == 1 && InventoryService.GetAmountOnInventory(moreBallsBooster) > 0)
+        {
+            ShowBoosterUsageRequestPopup(moreBallsBooster, null);
+        }
+    }
+
+    void ShowBoosterUsageRequestPopup(BoosterData boosterData, Action<bool> usageCallback)
+    {
+        var ui = Instantiate(boosterUsageRequestUIPrefab, uiContainer);
+        ui.Setup(boosterData, usageCallback);
+    }
+}
